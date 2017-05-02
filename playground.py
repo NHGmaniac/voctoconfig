29234414bbda2dfcb50721f0587e4a5ad5a85f5b
Benedikt Koblenz <nhg@posteo.de>
2017-05-02 12:08:03 +0200

    First Commit, added LIB and started playground

---
+++ playground.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import gi
+import signal
+import logging
+import sys
+import os
+import lib.connection as Connection
+from lib.loghandler import LogHandler
+from lib.args import Args
+
+def testCallback(args):
+    log = logging.getLogger("Test")
+    log.info(str(args))
+
+
+def main():
+    docolor = (Args.color == 'always') or (Args.color == 'auto' and
+                                           sys.stderr.isatty())
+    loghandler = LogHandler(docolor, Args.timestamp)
+    logging.root.addHandler(loghandler)
+    if Args.verbose >= 2:
+        level = logging.DEBUG
+    elif Args.verbose == 1:
+        level = logging.INFO
+    else:
+        level = logging.WARNING
+
+    logging.root.setLevel(level)
+    logging.debug('setting SIGINT handler')
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    Connection.establish(Args.host)
+    Connection.enterNonblockingMode()
+    Connection.on("test", testCallback)
+
+
+if __name__ == '__main__':
+    main()
